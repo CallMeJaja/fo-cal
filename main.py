1bde1dfa98d6df7f56ba4f3379614102f56c1dc6
CallMeJaja <fackyouu128@gmail.com>
2023-01-23 23:58:15 +0700

    app v1

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import os,time
+input('kamu masih jomblo? [y/n] : ')
+time.sleep(0.5)
 os.system('cls||clear')
 
-def Core():
-    
+def Core(): 
     core = int(input('\nInput Core Number => '))
     DiV = core / 12
     MoD = core % 12
     bulatAtas = round(DiV + 0.5)
-
     def findTube() :
         if bulatAtas == 1 or bulatAtas == 0:
             print('Biru')
@@ -34,7 +34,6 @@ def Core():
         elif bulatAtas == 0 or bulatAtas == 12 or bulatAtas == 13:
             print('Toska')
         return bulatAtas
-
     def findCore() :
         if MoD == 1:
             print('Biru')
@@ -61,11 +60,8 @@ def Core():
         elif MoD == 0 or MoD == 12:
             print('Toska')
         return MoD
-
-    os.system('cls||clear')
     time.sleep(0.2)
-
-    print('===== RESULT COLOR =====')
+    print('\n===== RESULT COLOR =====')
     print('Warna Tube dan Warna Core dari Core Ke-{} adalah \n'.format(core))
     print('Warna Tube :')
     findTube()
@@ -76,14 +72,24 @@ def Core():
     if q == "yes":
         Core()
     elif q == 'no':
-        print('Thank You For Use The Tools')
-        time.sleep(0.7)
-        exit()
+        time.sleep(0.5)
+        menu = int(input('\nPILIH MENU [0. MENU] => '))
+        if menu == 1:
+            Core()
+        elif menu == 2:
+            Color()
+        elif menu == 3:
+            About()
+        elif menu == 4:
+            exit()
+        elif menu == 0:
+            show_menu()
+        else:
+            print('')
 def Color():
     print('\n===== ALAT MENCARI CORE DARI WARNA =====')
     colorTube = input('masukan warna tube nya : ')
     colorCore = input('masukan warna core nya : ')
-
     def tube():
         global tube
         if colorTube == 'biru':
@@ -111,7 +117,6 @@ def Color():
         elif colorTube == 'toska':
             tube = (12-1)*12
         return tube
-
     def core():
         if colorCore == 'biru':
             core = 1 + tube()
@@ -138,53 +143,48 @@ def Color():
         elif colorCore == 'toska':
             core = 12 + tube()
         return core
-    os.system('cls||clear')
     time.sleep(0.5)
-    print('===== RESULT TOOLS =====')
+    print('\n===== RESULT TOOLS =====')
     print('Core Ke => ',core())
     q = input('\nIngin Mencoba lagi ? (yes/no) => ')
     if q == 'yes':
         Color()
     elif q == 'no':
-        time.sleep(0.2)
-        print('Thank You For Use Tools')
         time.sleep(0.5)
-        exit()
-
+        menu = int(input('\nPILIH MENU [0. BACK TO MENU] => '))
+        if menu == 1:
+            Core()
+        elif menu == 2:
+            Color()
+        elif menu == 3:
+            About()
+        elif menu == 4:
+            exit()
+        elif menu == 0:
+            show_menu()
+        else:
+            print('')
 def About():
-    os.system('cls||clear')
     time.sleep(0.5)
     print("""
-======================[ About EN-GEN ]======================
+==========================================[ ABOUT EN-GEN ]=================================================
                 
-                Name       : EN-GEN
-                Version    : 1.0.0
-                Date       : 2023-19-01: Purwakarta
-                Python Ver : 3.x.x
-                OS         : Windows
+                                    Name       : EN-GEN
+                                    Version    : 1.0.0
+                                    Date       : 2023-19-01: Purwakarta
+                                    Python Ver : 3.x
+                                    OS         : Windows
+                                    Author     : Reza Asriano Maulana
+                                    Instagram  : rezaasriano18
+                                    Github     : CallMeJaja
 
-============================================================
+===========================================================================================================
+──────────────────────────────── Donate for support me to upgrade this APP ────────────────────────────────
+──────────────────────────────── DANA : 085872792870 | OVO : 081217427056  ────────────────────────────────
+===========================================================================================================
 """)
     time.sleep(0.5)
-    exit()
-
-def show_menu():
-
-    print(("""
-    ===========[ APLIKASI FIBER OPTIC ]==========
-    
-    \t\t█▀▀ █▄░█ ▄▄ █▀▀ █▀▀ █▄░█
-    \t\t██▄ █░▀█ ░░ █▄█ ██▄ █░▀█
-    
-    =====[ Author : Reza Asriano Maulana ]=======
-    =============================================
-    """))
-    print('[ 1 ] FIND BY CORE')
-    print('[ 2 ] FIND BY COLOR')
-    print('[ 3 ] About Tools')
-    print('[ 4 ] EXIT')
-    menu = int(input('CHOOSE OPTION => '))
-
+    menu = int(input('PILIH MENU [0. MENU] => '))
     if menu == 1:
         Core()
     elif menu == 2:
@@ -192,9 +192,79 @@ def show_menu():
     elif menu == 3:
         About()
     elif menu == 4:
+        time.sleep(0.5)
+        print("""
+===========================================================================================================      
+=============================[ Terimakasih telah menggunakan aplikasi ini :) ]=============================      
+===========================================================================================================    
+        """)
+        time.sleep(1)
         exit()
+    elif menu == 0:
+        show_menu()
+    else:
+        time.sleep(0.5)
+        print('\n{} TIDAK ADA DI MENU. PILIH MENU 1 SAMPAI 4'.format(menu))
+        menu = int(input('\nPILIH MENU [9. BACK TO MENU] => '))
+        if menu == 1:
+            Core()
+        elif menu == 2:
+            Color()
+        elif menu == 3:
+            About()
+        elif menu == 4:
+           exit()
+        elif menu == 0:
+            show_menu()
+def show_menu():
+    print(("""
+====================================[ APILKASI FIBER OPTIC ]====================================
+────────────────────────────────────────────────────────────────────────────────────────────────                        
 
+                        _______  __    _        _______  _______  __    _ 
+                        |       ||  |  | |      |       ||       ||  |  | |
+                        |    ___||   |_| | ____ |    ___||    ___||   |_| |
+                        |   |___ |       ||____||   | __ |   |___ |       |
+                        |    ___||  _    |      |   ||  ||    ___||  _    |
+                        |   |___ | | |   |      |   |_| ||   |___ | | |   |
+                        |_______||_|  |__|      |_______||_______||_|  |__|
+
+──────────────────────────────────────────────────────────────────────────────── version 1.0 ──
+============================================================================== author : jaja ==
+    """))
+    print('MENU :\n')
+    print('[ 1 ] FIND BY CORE')
+    print('[ 2 ] FIND BY COLOR')
+    print('[ 3 ] ABOUT TOOLS')
+    print('[ 4 ] EXIT')
+    menu = int(input('\nPILIH MENU => '))
+    if menu == 1:
+        Core()
+    elif menu == 2:
+        Color()
+    elif menu == 3:
+        About()
+    elif menu == 4:
+        time.sleep(0.5)
+        print("""
+===========================================================================================================      
+=============================[ Terimakasih telah menggunakan aplikasi ini :) ]=============================      
+===========================================================================================================    
+        """)
+        time.sleep(1)
+        exit()
+    else:
+        time.sleep(0.5)
+        print('\n{} TIDAK ADA DI MENU. PILIH MENU 1 SAMPAI 4'.format(menu))
+        menu = int(input('\nPILIH MENU => '))
+        if menu == 1:
+            Core()
+        elif menu == 2:
+            Color()
+        elif menu == 3:
+            About()
+        elif menu == 4:
+           exit()
 if __name__ == "__main__":
-
     while(True):
         show_menu()
